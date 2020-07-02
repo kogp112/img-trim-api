1f3f6fb4a9f748bcbc13e7b0dd825828452258c1
kogp112 <gapkana@gmail.com>
2020-07-02 23:34:50 +0900

    add image size check

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM python:3
 
 ADD img_trim.py /
-
 RUN pip install Pillow matplotlib
-
+COPY ./test.jpg /
 CMD [ "python", "./img_trim.py" ]
--- img_trim.py
+++ img_trim.py
@@ -3,7 +3,8 @@ import matplotlib.image as mpimg
 
 # read local file
 def readFile():
-  img = mpimg.imread("./test.jpg", "r")
+  img = mpimg.imread("/test.jpg", "r")
   plt.imshow(img)
+  print(img.shape)
 
 readFile()
