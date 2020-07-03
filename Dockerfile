c4111e27992e2f993ecf5645ecc8c599d37596a5
kogp112 <gapkana@gmail.com>
2020-07-03 20:23:24 +0900

    modify image cut function

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3
 
 ADD img_trim.py /
-RUN pip install Pillow matplotlib
+RUN pip install Pillow
 COPY ./test.jpg /
 CMD [ "python", "./img_trim.py" ]
--- img_trim.py
+++ img_trim.py
@@ -1,10 +1,20 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+import os, sys
+from PIL import Image
 
-# read local file
-def readFile():
-  img = mpimg.imread("/test.jpg", "r")
-  plt.imshow(img)
-  print(img.shape)
-
-readFile()
+# divide image file
+def divideFile():
+  img = Image.open("./test.jpg", "r")
+  img.show()
+  
+  # print image size in pixels
+  width = img.size[0]
+  height = img.size[1]
+  
+  # cut image (left, upper, right, lower)
+  box = (0, 0, width / 2, height)
+  print(box)
+  croppedImage = img.crop(box)
+  outfile = "./croppedTest.jpg"
+  croppedImage.save(outfile, "JPEG")
+  
+divideFile()
